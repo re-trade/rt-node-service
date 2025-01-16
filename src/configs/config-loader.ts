d7e96fbf632b9b0157185fd6d9114a13b43eae69
AnataArisa <92080339+AnataAria@users.noreply.github.com>
2025-01-16 18:41:42 +0700

    improve loader

--- src/configs/config-loader.ts
+++ src/configs/config-loader.ts
@@ -1,11 +1,15 @@
-import EnvLoader from "../helpers/env-loader.js"
+import EnvLoader, { TEnvValidatorKeyMap } from "../helpers/env-loader.js"
 
 type TSeaEnvironment = {
     NODE_ENV: 'development' | 'production',
     PORT: number,
     USER_API_URL: string
 }
-
-const configLoader = new EnvLoader<TSeaEnvironment>();
+const validators: TEnvValidatorKeyMap<TSeaEnvironment> = {
+    NODE_ENV: { required: true, default: 'development' },
+    PORT: { required: true, default: 3000 },
+    USER_API_URL: { required: false }
+};
+const configLoader = new EnvLoader<TSeaEnvironment>(validators);
 
 export default configLoader;
--- src/helpers/env-loader.ts
+++ src/helpers/env-loader.ts
@@ -6,6 +6,14 @@ type TEnvKeyMap<DataType> = {
     [Property in keyof DataType]: TEnvKeyType<DataType, Property>;
 }
 
+export type TEnvValidator<DataType, K extends keyof DataType> = {
+    required: boolean;
+    default?: TEnvKeyType<DataType, K>;
+}
+
+export type TEnvValidatorKeyMap<DataType> = {
+    [Property in keyof DataType]: TEnvValidator<DataType,Property>
+}
 const envConfigOptionLoader = (option?: DotenvConfigOptions): void => {
     if (option) {
         config(option);
@@ -16,21 +24,33 @@ const envConfigOptionLoader = (option?: DotenvConfigOptions): void => {
 export class EnvLoader<T extends {}> {
     private env: TEnvKeyMap<T> = {} as TEnvKeyMap<T>;
 
-    constructor(postProcess?: (env: TEnvKeyMap<T>) => TEnvKeyMap<T>) {
+    constructor(validators: TEnvValidatorKeyMap<T>, postProcess?: (env: TEnvKeyMap<T>) => TEnvKeyMap<T>) {
         envConfigOptionLoader();
         const envList = process?.env;
 
         if (!envList) {
             throw new Error(`Can't load config`);
         }
-        for (const key in envList) {
-            if (envList.hasOwnProperty(key)) {
+
+        for (const key in validators) {
+            if (validators.hasOwnProperty(key)) {
+                const validator = validators[key];
                 const value = envList[key];
-                if (value !== undefined) {
+
+                if (value !== undefined && value !== '') {
                     this.env[key as keyof T] = value as TEnvKeyType<T, keyof T>;
-                    console.log(`Add ENV with name ${key}: Success`);
+                    console.log(`Environment variable "${key}" loaded successfully.`);
                 } else {
-                    console.log(`ENV variable ${key} is undefined`);
+                    if (validator.required) {
+                        throw new Error(`Required environment variable "${key}" is missing.`);
+                    } else {
+                        if (validator.default !== undefined) {
+                            this.env[key as keyof T] = validator.default as TEnvKeyType<T, keyof T>;
+                            console.log(`Environment variable "${key}" is missing. Using default value.`);
+                        } else {
+                            console.log(`Environment variable "${key}" is missing, and no default value is set.`);
+                        }
+                    }
                 }
             }
         }
